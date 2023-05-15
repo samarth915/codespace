7b1396158995fb0a0881b1aa7266c344c54f2d73
samarth915 <129299053+samarth915@users.noreply.github.com>
2023-05-15 06:27:44 +0000

    Mon, May 15, 2023, 6:27 AM +00:00

---
+++ try.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+bool is_even(int n)
+{
+  if(n>0)
+  {
+    if ( n%2==0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+}
+
+
+bool issum(int n)
+{
+    int sum;
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n>0)
+    {
+        sum = n%10 + issum(n/10);
+    }
+    return is_even(sum);
+}
+
